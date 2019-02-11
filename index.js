80396cacff2947badcc889f2b0158ec929d99bdf
Angel Torres <alas4224@gmail.com>
2019-02-11 16:47:34 -0600

    finished post request and get all users request

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // implement your API here
+
+const express = require('express');
+const db = require('./data/db');
+const server = express();
+
+server.use(express.json());
+
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    if (!user.name && !user.bio) {
+       res.status(400).json({ error: "Please provide name and bio for the user." }) 
+    } else {
+        db
+        .insert(user)
+        .then( user => {
+            res.status(201).json({ success: true, user})
+        })
+        .catch(err => {
+            res.status(500).json({ error: "There was an error while saving the user to the database" })
+        })
+    }
+})
+
+server.get('/api/users', (req, res) => {
+    db
+    .find()
+    .then(users => {
+        res.status(200).json({success: true, users})
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The users information could not be retrieved." })
+    })
+
+})
+
+server.listen(4000, () => {
+    console.log('\n *** server listening *** \n')
+})
