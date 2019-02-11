d45a57e6e6a7f40e0b8fbf2bc276e7aec5ba2a6a
Angel Torres <alas4224@gmail.com>
2019-02-11 17:46:51 -0600

    added ability to delete a user

--- index.js
+++ index.js
@@ -47,6 +47,25 @@ server.get('/api/users/:id', (req, res) => {
     })
 })
 
+server.delete('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+    
+    db
+    .remove(userId)
+    .then( deleted => {
+        if (deleted) {
+            res.status(200).json({success: true})
+        } else {
+            res.status(404).json({ message: "The user with the specified ID does not exist." })
+        }
+    })
+    .catch( err => {
+        res.status(500).json({ error: "The user could not be removed" })
+    })
+})
+
+
+
 server.listen(4000, () => {
     console.log('\n *** server listening *** \n')
 })
