42ef4185c23f6f55254b1e9bbe8e60026aee05ce
Angel Torres <alas4224@gmail.com>
2019-02-11 18:16:32 -0600

    added ability to update user

--- index.js
+++ index.js
@@ -64,7 +64,27 @@ server.delete('/api/users/:id', (req, res) => {
     })
 })
 
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const user = req.body;
 
+    if (!user.name && !user.bio) {
+        res.status(400).json({ error: "Please provide name and bio for the user." }) 
+        } else {
+            db
+            .update(id, user)
+            .then( user => {
+                if(user) {
+                    res.status(200).json({success: true, user})
+                } else {
+                    res.status(404).json({ message: "The user with the specified ID does not exist." })
+                }
+            })
+            .catch( err => {
+            res.status(500).json({ error: "There was an error while saving the user to the database" })
+        })
+        }
+    })
 
 server.listen(4000, () => {
     console.log('\n *** server listening *** \n')
